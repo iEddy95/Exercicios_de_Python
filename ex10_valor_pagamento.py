faa1ea65a74d4940bba3606875848b600b63360b
iEddy95 <101272251+iEddy95@users.noreply.github.com>
2022-03-31 16:11:12 -0300

    Update ex10_valor_pagamento.py

--- ex10_valor_pagamento.py
+++ ex10_valor_pagamento.py
@@ -1,22 +1,23 @@
-valorprestacao = []
-valorPagamento = 0
-cont = 0
+q = t = parcela = dias = valorPagamento = 0
 
-while valorprestacao != 0:
-    valorprestacao = float(input('Informe o valor da prestação: '))
-    if valorprestacao == 0:
-        print(cont)
-        print(valorprestacao)
+while True:
+    parcela = float(input('Valor da prestação: '))
+    if parcela == 0:
+        print('#'*36)
+        print('#         RELATÓRIO DO DIA         #')
+        print('#'*36)
+        print(f'# Valor total das parcelas: R${t:.2f}\n# Quantidade de parcelas: {q} ')
+        print('#' * 36)
         break
-    dias = int(input('Dias atrasada: '))
-    if dias > 0:
-        multadia = 0.001 * dias
-        multa = valorprestacao * 0.03
-        valorPagamento = valorprestacao + multadia + multa
-        valorprestacao.append(valorPagamento)
-        cont += 1
+    dias = int(input('Dias em atraso: '))
+    if dias == 0:
+        valorPagamento = parcela
+        print(f'Valor a ser pago: {valorPagamento}')
+        print('¨¨¨' * 10)
     else:
-        valorPagamento = valorprestacao
-        valorprestacao.append(valorPagamento)
-        cont += 1
-    print(f'Print valor a ser pago: {valorPagamento:.2f}')
+        multa = parcela * 0.03 + 0.001 * dias
+        valorPagamento = parcela + multa
+        print(f'Valor a ser pago: {valorPagamento:.2f}')
+        print('¨¨¨'*10)
+    q += 1
+    t += valorPagamento
